1e8cd80404372e4636bd357e05dfadadcb16d794
zaslavsky-dima <zaslasvky.dima20@gmail.com>
2020-05-11 19:14:52 +0300

    Spider Cosmetic.com.ua
    Override start method

--- cosmetics_crawler/spiders/cosmetic_com_ua.py
+++ cosmetics_crawler/spiders/cosmetic_com_ua.py
@@ -1,11 +1,46 @@
 # -*- coding: utf-8 -*-
+from typing import List, Union
+
 import scrapy
+from scrapy import Request
+
+SCRAPED_CATEGORIES = [
+    # 'dekorativnaya-kosmetika',
+    'kosmetika-po-ukhodu',
+]
 
 
 class CosmeticComUaSpider(scrapy.Spider):
     name = 'cosmetic_com_ua'
     allowed_domains = ['cosmetic.com.ua']
-    start_urls = ['http://cosmetic.com.ua/']
+    start_urls = ['http://cosmetic.com.ua']
+
+    def start_requests(self):
+        for category in SCRAPED_CATEGORIES:
+            url = f'{self.start_urls[0]}/{category}'
+            yield Request(url=url)
 
     def parse(self, response):
-        pass
+        product_listing_urls = response.xpath('//a[@class="fwd"]/@href').extract()
+        for request in self.send_requests(product_listing_urls, self.parse_product_listing):
+            yield request
+
+    def parse_product_listing(self, response):
+        product_urls = response.xpath('//div[@class="title"]/a/@href').extract()
+        for request in self.send_requests(product_urls, self.parse_product):
+            yield request
+
+    def parse_product(self, response):
+        breadcrumbs = response.xpath('//nav[@class="breadcrumbs"]//a//text()').extract()[1:]
+        title = response.xpath('//h1//text()').extract_first()
+
+        return {
+            'breadcrumbs': breadcrumbs,
+            'title': title,
+        }
+
+    def send_requests(self, urls: List[str], method_name) -> Union:
+        for url in urls:
+            if self.name not in url:
+                url = f'{self.start_url}{url}'
+                yield Request(url=url, callback=method_name)
